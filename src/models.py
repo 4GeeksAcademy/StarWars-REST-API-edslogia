a07ff758245b1c48b9e16cf45739deaf072e98c4
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-09 17:22:00 +0000

    modelo de base de datos funcionando

--- src/models.py
+++ src/models.py
@@ -1,8 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey, Enum, Date, Time, DateTime, Integer, Numeric, Column, Table
+from sqlalchemy import String, Boolean, ForeignKey, Integer, Numeric, Column, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from eralchemy2 import render_er
-from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -29,12 +27,12 @@ user_people_favorites = Table(
 
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(120), nullable=False)
-    age: Mapped[int] = mapped_column(Integer(), nullable=False)
-    nickname:Mapped[str] = mapped_column(String(16), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)  
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+    nickname: Mapped[str] = mapped_column(String(16), nullable=False)
+    is_active: Mapped[bool] = mapped_column(nullable=False)  
     favorite_planets: Mapped[list["Planets"]] = relationship("Planets", secondary=user_planet_favorites, back_populates="fans")
     favorite_species: Mapped[list["Species"]] = relationship("Species", secondary=user_species_favorites, back_populates="fans")
     favorite_people: Mapped[list["People"]] = relationship("People", secondary=user_people_favorites, back_populates="fans")
@@ -42,20 +40,21 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,            
+            "email": self.email,       
+            "age": self.age,
             "nickname": self.nickname,
-            "favorite_planet" : self.favorite_planet,
-            "favorite_species" : self.favorite_species,
-            "favorite_people" : self.favorite_people
+            "favorite_planets": [planet.serialize() for planet in self.favorite_planets],
+            "favorite_species": [specie.serialize() for specie in self.favorite_species],
+            "favorite_people": [person.serialize() for person in self.favorite_people]
         }
 
 class Planets(db.Model):
-    id: Mapped[int] = mapped_column(Integer(),primary_key=True)
-    name: Mapped[str] = mapped_column(String(80), nullable=False)
-    climate: Mapped[str] = mapped_column(String(80), nullable=False)
-    diameter_km: Mapped[float] = mapped_column(Numeric(20,2), nullable=False)
-    population: Mapped[int] = mapped_column(Integer(), nullable=False)
-    terrain: Mapped[str] = mapped_column(String(80), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    diameter_km: Mapped[float] = mapped_column(nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
     fans: Mapped[list["User"]] = relationship("User", secondary=user_planet_favorites, back_populates="favorite_planets")
 
     def serialize(self):
@@ -106,10 +105,3 @@ class People(db.Model):
             "gender": self.gender,
             "height_cm": self.height_cm
         }  
-
-
-try:
-    render_er(db.Model, 'diagram.png')
-    print("✅ Diagrama generado correctamente como diagram.png")
-except Exception as e:
-    print("❌ Error generando el diagrama:", e)
